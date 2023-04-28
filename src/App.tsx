c956c4c251b8ed36941f82aceb52adc385e012e8
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-04-28 12:21:45 -0300

    code cleanup and initial CSS setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Casa Verde</p>
+      <h1>Título 1</h1>
+      <h2>Título 2</h2>
+      <h3>Título 3</h3>
     </div>
   );
 }
