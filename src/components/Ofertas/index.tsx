2b711259c18ea980a00d3be37272533c2bb4083c
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-05-04 12:50:20 -0300

    apply sort and filter

--- src/components/Ofertas/index.tsx
+++ src/components/Ofertas/index.tsx
@@ -1,30 +1,95 @@
 import { useEffect, useState } from "react";
 import Card from "../Card";
 import Heading from "../Heading";
-import { Cards, Title, Wrapper } from "./styled";
+import { Cards, Empty, Options, Title, Wrapper } from "./styled";
 import Product from "../../interfaces/Product";
 import getProducts from "../../utils/api";
 
 export default function Ofertas() {
-    const [products, setProducts] = useState<Product[]>([])
+    // I need to keep a list of all products in order to undo filter/sort operations
+    const [allProducts, setAllProducts] = useState<Product[]>([]);
+    const [products, setProducts] = useState<Product[]>([]);
+    const [price, setPrice] = useState('');
+    const [sortType, setSortType] = useState('');
+    const [filterValue, setFilterValue] = useState('');
 
+    // load products
     useEffect(() => {
         async function fetchData() {
             const data = await getProducts();
             if (data) {
-                setProducts(data)
+                setProducts(data);
+                setAllProducts(data);
             }
         }
         fetchData();
     }, [])
 
+    // apply sort and filter
+    useEffect(() => {
+        function orderBy(list: Product[]): Product[] {
+            switch (sortType) {
+                // sort modifies the original array, so first we need to make a copy of it 
+                case 'price':
+                    return [...list].sort((a, b) => a.price >= b.price ? 1 : -1);
+                case 'name':
+                    return [...list].sort((a, b) => a.name >= b.name ? 1 : -1);
+            }
+            return list;
+        }
+    
+        function filterByPrice(): Product[] {
+            const value = parseFloat(filterValue);
+            if (!isNaN(value) && value >= 0) {
+                return allProducts.filter(item => item.price <= value);
+            }
+            else {
+                return allProducts;
+            }
+        }
+
+        const list = orderBy(filterByPrice())
+        setProducts(list);
+    }, [filterValue, sortType, allProducts])
+
+    function changeFilter(e:React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        setFilterValue(price);
+    }
+
     return (
         <Wrapper>
             <Title>
-                <Heading subText="Conheça nossas" mainText="ofertas" size="large" />
+                <Heading subText="Conheça nossas" mainText="plantas" size="large" />
             </Title>
+            <Options>
+                <div>
+                    <label htmlFor="orderby">Ordernar por:</label>
+                    <select id='orderby' onChange={e => setSortType(e.target.value)}>
+                        <option>Ordenação padrão</option>
+                        <option value='price'>Preço</option>
+                        <option value='name'>Nome</option>
+                    </select>
+                </div>
+                <div>
+                    <form onSubmit={changeFilter}>
+                        <label htmlFor="filter">Mostrar produtos até R$ </label>
+                        <input 
+                            type="number" 
+                            min="0" 
+                            placeholder="Digite um valor" 
+                            value={price}
+                            onChange={e => setPrice(e.target.value)}
+                        />
+                        <button>OK</button>
+                    </form>
+                </div>
+            </Options>
             <Cards>
-                {products.map(product => product.quantity > 0 && <Card key={product.id} {...product} />)}
+                {products.map(product => (
+                    product.quantity > 0 && <Card key={product.id} {...product} />
+                ))}
+                {products.length === 0 && <Empty>Nenhum produto para mostrar.</Empty>}
             </Cards>
         </Wrapper>
     )
--- src/components/Ofertas/styled.tsx
+++ src/components/Ofertas/styled.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.section`
     max-width: 90%;
+    min-height: 70vh;
     margin: auto;
     padding: 3rem 0;
 `
@@ -15,4 +16,38 @@ export const Cards = styled.div`
     flex-wrap: wrap;
     justify-content: space-around;
     gap: 1.8rem;
+`
+
+export const Options = styled.div`
+    display: flex;
+    justify-content: space-around;
+    margin: 1.5rem auto;
+    & label {
+        margin-right: .5rem;
+    }
+    & select, input, button {
+        font-family: var(--text);
+        font-size: 1rem;
+        font-weight: 400;    
+        padding: 1rem;
+        box-sizing: border-box;        
+    }
+    & select, input {
+        border: 1px solid var(--white);
+        &:hover {
+            border: 1px solid var(--orange);
+        }
+    }
+    & button {
+        border: 1px solid var(--orange);
+        background-color: var(--orange);
+        color: var(--white);
+        &:hover {
+            background-color: var(--orange-hover)
+        }
+    }
+`
+
+export const Empty = styled.p`
+    padding: 4rem 0 2rem;
 `
